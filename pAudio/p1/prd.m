ff53d670dc7a8b179df5c6e866d1eb4016280132
david <d.santa.2021@alumnos.urjc.es>
2024-10-18 14:10:27 +0200

    p1 audio

---
+++ pAudio/p1/prd.m
@@ -0,0 +1,20 @@
+function score = prd(ref_audio, ev_audio)
+% Carga de los archivos de audio.
+[reference, fs1] = audioread(ref_audio);
+[degraded, fs2] = audioread(ev_audio);
+reference = abs(reference);
+degraded = abs(degraded);
+
+% Almacenamiento de la longitud de la grabación.
+ref_len = length(reference)/fs1;
+
+% Remuestreo para poder realizar la comparativa con igual fs.
+if fs1~=fs2
+    degraded = resample(degraded,fs1,fs2);
+end
+% Obtención del PRD
+score = 100 * sqrt(sum(((reference - degraded).^2))/sum(reference.^2));
+
+end
+
+ 
